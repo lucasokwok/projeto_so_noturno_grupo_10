e9bc473afaa23a233ef940a4f48ba140981fcfaa
Lucas Kwok <lucas.o.kwok@gmail.com>
2025-07-02 18:37:05 -0300

    Corrige expected expression em enqueue() caso default

--- minix/kernel/proc.c
+++ minix/kernel/proc.c
@@ -1611,78 +1611,78 @@ void enqueue(register struct proc *rp /* this process is now runnable */)
 
   assert(proc_is_runnable(rp));
 
-  switch (escalonador) {
-    case 1: // FCFS (FIFO)
-      rp->p_nextready = NULL;
+  	switch (escalonador) {
+		case 1: // FCFS (FIFO)
+		rp->p_nextready = NULL;
 
-      if (fifo_fim != NULL)
-        fifo_fim->p_nextready = rp;// se ja tem processo na fila vai pro final
-      else
-        fifo_inicio = rp;// senao processo vai pra primeiro da fila
+		if (fifo_fim != NULL)
+			fifo_fim->p_nextready = rp;// se ja tem processo na fila vai pro final
+		else
+			fifo_inicio = rp;// senao processo vai pra primeiro da fila
 
-      fifo_fim = rp;
-      break;
+		fifo_fim = rp;
+		break;
 
-    case 2: // Round Robin (RR)
-      // ainda não implementado
-      break;
+		case 2: // Round Robin (RR)
+		// ainda não implementado
+		break;
 
-    case 3: // Prioridade 
-      // ainda não implementado
-      break;
+		case 3: // Prioridade 
+		// ainda não implementado
+		break;
 
-    default:
-      /* Comportamento padrão do Minix (Round-Robin com múltiplas filas) */
-      int q = rp->p_priority;	 		/* scheduling queue to use */
+		default: {
+			int q = rp->p_priority;	 		/* scheduling queue to use */
 
-      assert(q >= 0);
+			assert(q >= 0);
 
-      rdy_head = get_cpu_var(rp->p_cpu, run_q_head);
-      rdy_tail = get_cpu_var(rp->p_cpu, run_q_tail);
+			rdy_head = get_cpu_var(rp->p_cpu, run_q_head);
+			rdy_tail = get_cpu_var(rp->p_cpu, run_q_tail);
 
-      /* Now add the process to the queue. */
-      if (!rdy_head[q]) {		/* add to empty queue */
-          rdy_head[q] = rdy_tail[q] = rp; 		/* create a new queue */
-          rp->p_nextready = NULL;		/* mark new end */
-      } 
-      else {					/* add to tail of queue */
-          rdy_tail[q]->p_nextready = rp;		/* chain tail of queue */	
-          rdy_tail[q] = rp;				/* set new queue tail */
-          rp->p_nextready = NULL;		/* mark new end */
-      }
+			/* Now add the process to the queue. */
+			if (!rdy_head[q]) {		/* add to empty queue */
+				rdy_head[q] = rdy_tail[q] = rp; 		/* create a new queue */
+				rp->p_nextready = NULL;		/* mark new end */
+			} 
+			else {					/* add to tail of queue */
+				rdy_tail[q]->p_nextready = rp;		/* chain tail of queue */	
+				rdy_tail[q] = rp;				/* set new queue tail */
+				rp->p_nextready = NULL;		/* mark new end */
+			}
 
-      if (cpuid == rp->p_cpu) {
-          /*
-           * enqueueing a process with a higher priority than the current one,
-           * it gets preempted. The current process must be preemptible. Testing
-           * the priority also makes sure that a process does not preempt itself
-           */
-          struct proc * p;
-          p = get_cpulocal_var(proc_ptr);
-          assert(p);
-          if((p->p_priority > rp->p_priority) &&
-                  (priv(p)->s_flags & PREEMPTIBLE))
-              RTS_SET(p, RTS_PREEMPTED); /* calls dequeue() */
-      }
-    #ifdef CONFIG_SMP
-      /*
-       * if the process was enqueued on a different cpu and the cpu is idle, i.e.
-       * the time is off, we need to wake up that cpu and let it schedule this new
-       * process
-       */
-      else if (get_cpu_var(rp->p_cpu, cpu_is_idle)) {
-          smp_schedule(rp->p_cpu);
-      }
-    #endif
+			if (cpuid == rp->p_cpu) {
+				/*
+				* enqueueing a process with a higher priority than the current one,
+				* it gets preempted. The current process must be preemptible. Testing
+				* the priority also makes sure that a process does not preempt itself
+				*/
+				struct proc * p;
+				p = get_cpulocal_var(proc_ptr);
+				assert(p);
+				if((p->p_priority > rp->p_priority) &&
+						(priv(p)->s_flags & PREEMPTIBLE))
+					RTS_SET(p, RTS_PREEMPTED); /* calls dequeue() */
+			}
+			#ifdef CONFIG_SMP
+				/*
+				* if the process was enqueued on a different cpu and the cpu is idle, i.e.
+				* the time is off, we need to wake up that cpu and let it schedule this new
+				* process
+				*/
+				else if (get_cpu_var(rp->p_cpu, cpu_is_idle)) {
+					smp_schedule(rp->p_cpu);
+				}
+			#endif
 
-      /* Make note of when this process was added to queue */
-      read_tsc_64(&(get_cpulocal_var(proc_ptr)->p_accounting.enter_queue));
+			/* Make note of when this process was added to queue */
+			read_tsc_64(&(get_cpulocal_var(proc_ptr)->p_accounting.enter_queue));
 
-    #if DEBUG_SANITYCHECKS
-      assert(runqueues_ok_local());
-    #endif
-      break;
-  }
+			#if DEBUG_SANITYCHECKS
+				assert(runqueues_ok_local());
+			#endif
+			break;
+		}
+	}
 }
 
 /*===========================================================================*
